cb3de7c046e2cc7c9075b99d87988a8eb8360150
hbhatt <hbhatt@ipcoop.com>
2019-12-24 11:22:33 -0500

    initial commit

---
+++ NetCoreWebApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreWebApp.Models;
+
+namespace NetCoreWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.IpAddress = HelperClass.GetLocalIPAddress();
+            ViewBag.HostName = HelperClass.GetHostName();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+public static class HelperClass
+{
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    public static string GetHostName()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        return host.HostName;
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+}
